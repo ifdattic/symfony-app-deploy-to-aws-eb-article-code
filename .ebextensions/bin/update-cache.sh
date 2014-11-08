fe0ed8aa03012863af506f557b8169e8b99804e4
Andrew M <andrew.web@ifdattic.com>
2014-11-08 10:15:43 +0100

    Update EB deploy script (replacing cache "ondeck" to "current")
    From: http://ifdattic.com/how-to-deploy-symfony-application-to-aws-elasticbeanstalk/#comment-1655573334

--- .ebextensions/bin/update-cache.sh
+++ .ebextensions/bin/update-cache.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sed -i -e "s/\/var\/app\/ondeck/\/var\/app\/current/" app/cache/$SYMFONY_ENV/*.php
+for i in $(grep -l -R "ondeck" /var/app/ondeck/app/cache/$SYMFONY_ENV/*); do
+    sed -i -e "s/\/var\/app\/ondeck/\/var\/app\/current/g" $i
+done
